6d114edeb193877ac33ef9c45bc3579600505da8
Sabinus52 <sabinus52@gmail.com>
2019-01-14 14:11:18 +0100

    Request /info

---
+++ src/Component/Network.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Informations réseau de l'enceinte
+ *
+ * @author Olivier <sabinus52@gmail.com>
+ *
+ * @package SoundTouchApi
+ */
+
+namespace Sabinus\SoundTouch\Component;
+
+
+class Network
+{
+
+    private $macAddress;
+
+    private $ipAddress;
+
+
+    /**
+     * Contructeur
+     * 
+     * @param SimpleXMLElement $xml : Xml de la réponse
+     */
+    public function __construct($xml)
+    {
+        $this->macAddress = strval($xml->macAddress);
+        $this->ipAddress = strval($xml->ipAddress);
+    }
+
+
+    /**
+     * @return String
+     */
+    public function getMacAddress()
+    {
+        return $this->macAddress;
+    }
+
+    /**
+     * @return String
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
+    }
+    
+}
--- test/example.php
+++ test/example.php
@@ -5,3 +5,13 @@ require 'bootstrap.php';
 use Sabinus\SoundTouch\SoundTouchApi;
 
 $api = new SoundTouchApi('soundtouch');
+
+// Info
+$result = $api->getInfo();
+print '--- Informations ---------------------------------------------------------------------------'."\n";
+print 'Nom : '.$result->getName()."\n";
+print 'Type : '.$result->getType()."\n";
+print 'Compte : '.$result->getAccount()."\n";
+print 'Mac : '.$result->getNetwork()->getMacAddress()."\n";
+print 'IP : '.$result->getNetwork()->getIpAddress()."\n";
+print "\n";
